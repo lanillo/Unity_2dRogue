f1682b7e2db23adb86760692b1a3c28102815d10
lanillo <luis.felipe.anillo@usherbrooke.ca>
2017-05-21 10:41:49 -0400

    We need to restart

---
+++ CodeRogue/Rogue_2D_Tuto/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null; //For Singleton Class
+    public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
+
+
+
+    private int level = 3;
+
+	// Use this for initialization
+	void Awake () {
+
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject); //Does not destroy between scenes
+
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+		
+	}
+
+    void InitGame() {
+
+        boardScript.SetupScene(level);
+    
+    }
+
+    void GameOver() {
+
+        enabled = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
